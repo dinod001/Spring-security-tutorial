1177faf59c3bf514bfd2fc83fee71c06f4922dad
DSYS <dinodimanjith404@gmail.com>
2025-02-09 07:11:17 +0530

    Dao Authentication Proivder

---
+++ Day 02 DaoAuthentication/src/main/java/com/example/security/Service/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.security.Service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public MyUserDetailsService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails user=User.builder()
+                .username("dinod")
+                .password(passwordEncoder.encode("dinod"))
+                .build();
+        return user;
+    }
+}
